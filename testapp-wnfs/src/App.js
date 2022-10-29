eb3fc3feb4aa8c5d72739cc087a40c7c1cdf96eb
ehsan shariati <ehsan6sha@gmail.com>
2022-10-29 10:55:25 -0400

    added packages

--- testapp-wnfs/src/App.js
+++ testapp-wnfs/src/App.js
@@ -1,8 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import {useEffect} from 'react';
-import { MemoryBlockStore } from 'ipfs-car/blockstore/memory' 
+import { MemoryBlockStore } from 'ipfs-car/blockstore/memory';
+import { MemoryBlockstore as Blockstore } from 'blockstore-core/memory';
+import { MemoryDatastore } from 'datastore-core/memory';
+import { BlockstoreDatastoreAdapter } from 'blockstore-datastore-adapter';
 import init, { PrivateDirectory, PrivateForest, Namefilter } from "fx-wnfs";
+import { CID } from 'multiformats/cid';
+import * as json from 'multiformats/codecs/json';
+import { sha256 } from 'multiformats/hashes/sha2';
 var sjcl = require('randombytes');
 
 const cid = Uint8Array.from([
@@ -16,17 +22,53 @@ function App() {
     //
 
     const store = new MemoryBlockStore();
+    const store2 = new Blockstore();
+    const store3 = new BlockstoreDatastoreAdapter(new MemoryDatastore());
+    const store4 = {data:{}};
+
     store.putBlock = (cid, data) => {
+      console.log({cid: cid, data: data});
       return store.put(cid, data);
     }
     store.getBlock = store.get;
+
+    store2.putBlock = (cid, data) => {
+      console.log({cid: cid, data: data});
+      return store2.put(cid, data);
+    }
+    store2.getBlock = (cid) => {
+      console.log({cid: cid,});
+      return store2.get(cid);
+    }
+
+    store3.putBlock = (cid, data=null, options=null) => {
+      console.log("put", {cid: cid, data: data, options:options});
+      return store3.put(cid, data);
+    }
+    store3.getBlock = (cid, options=null) => {
+      console.log("get",{cid: cid, options:options});
+      return store3.get(cid);
+    }
+
+    store4.putBlock = async (data=null, codec, options=null) => {
+      console.log("put", {codec: codec, data: data, options:options});
+      let hash = await sha256.digest(data)
+      let cid = CID.create(1, json.code, hash)
+      store4.data[cid] = data;
+      return cid;
+    }
+    store4.getBlock = (cid, options=null) => {
+      console.log("get",{cid: cid, options:options});
+      return store4.data[cid];
+    }
+    
     useEffect(() => {
        init().then(() => {
     
        
-         const initialHamt = new PrivateForest();
-         const dir = new PrivateDirectory(new Namefilter(), time, rng);
-         const fetchData = async () => {
+        const initialHamt = new PrivateForest();
+        const dir = new PrivateDirectory(new Namefilter(), time, rng);
+        const fetchData = async () => {
         //START
         console.log(dir);
         var { rootDir, hamt } = await dir.mkdir(
@@ -34,7 +76,7 @@ function App() {
           true,
           time,
           initialHamt,
-          store,
+          store4,
           rng
         );
 
